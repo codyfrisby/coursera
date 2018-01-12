492fc6bf1f1962ff8940315d2c62719512497cf1
Cody Frisby <codyfrisby@gmail.com>
2018-01-11 21:06:20 -0700

    starting ch03

---
+++ python/pyML/ch03.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 20:21:42 2018
+
+@author: codyfrisby
+"""
+
+from sklearn import datasets
+import numpy as np
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+np.unique(y) # to see how many unique values there are
+
+# split into test and train
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# standardize
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# Use Perceptron from sklearn to fit the model
+from sklearn.linear_model import Perceptron
+ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+
+# predict and test
+y_pred = ppn.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+
+# percent error
+#((y_test != y_pred).sum() / len(y_pred)) * 100
+
+# acuracy
+#1 - ((y_test != y_pred).sum() / len(y_pred))
+
+# use scikit-learn built in accuracy function:
+from sklearn.metrics import accuracy_score
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+
+
+# Plot Decision Boundry function from chapter 2
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
+                        np.arange(x2_min, x2_max, resolution))
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    z = z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    
+    # plot class samples
+    for idx, c1 in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == c1, 0], y=X[y == c1, 1], alpha=0.8, 
+                    c=cmap(idx), marker=markers[idx], label=c1)
+# end of decision boundry function
+
